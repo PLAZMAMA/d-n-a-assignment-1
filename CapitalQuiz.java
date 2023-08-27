ff5ced09ebb0ea6e9d5888557c18c13653583b92
Maor Cohen <maorioriori@gmail.com>
2023-08-27 19:54:13 -0400

    rename and add function header

---
+++ CapitalQuiz.java
@@ -0,0 +1,9 @@
+class CapitalQuiz {
+    static final 
+    public static String[][] bubbleSort(String[][] arr, int key_indx) {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
